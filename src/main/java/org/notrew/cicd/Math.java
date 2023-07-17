d1e17e6d0757eb5d4648a05d2739d02c14db8650
werton <mr.werton@gmail.com>
2023-07-17 04:17:19 -0300

    feat: created math class

---
+++ src/main/java/org/notrew/cicd/Math.java
@@ -0,0 +1,19 @@
+package org.notrew.cicd;
+
+public class Math {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+}
